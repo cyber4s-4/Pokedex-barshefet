b29b7c2ce72d3938789478e582e1b8b4aca5def8
barshefet <bar.shefet@gmail.com>
2022-07-18 16:32:01 +0300

    added server

---
+++ Pokedex/src/server/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+
+const port = 4200
+
+app.use(cors());
+
+const root = path.join(process.cwd(), 'dist');
+
+app.use(express.static(root));
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(root, 'index.html'));
+});
+
+
+app.get('/pokemon', (req, res)=>{
+    res.send('pikachu')
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
+
+
+
+// nodemon
+// server folder
