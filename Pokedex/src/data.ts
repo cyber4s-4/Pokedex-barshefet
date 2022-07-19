b396c751b1ad943790fad28f99da5b9a942486c2
barshefet <bar.shefet@gmail.com>
2022-07-19 20:39:30 +0300

    asd

--- Pokedex/src/data.ts
+++ Pokedex/src/data.ts
@@ -1,6 +1,16 @@
+
+export let pokeList : any[] = []
+
 export function Dman(howMany: number) {
-    for( let i = 0; i < howMany; i++){
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(res => res.json()).then(data => console.log(data))
-    }
+  for( let i = 1; i < howMany; i++){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then(res => res.json())
+    .then(data=> {pokeList.push(data)})
   }
+
+  console.log(pokeList)
+
+}
+Dman(200)
+
  
--- Pokedex/src/server/mongo.js
+++ Pokedex/src/server/mongo.js
@@ -1,16 +1,16 @@
-const { MongoClient, Db } = require("mongodb");
-
-
+import { MongoClient } from "mongodb";
 
 const uri = `mongodb+srv://barshefet:ilovecode@cluster0.rxb57.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
-let pokeList = [];
+let pokeList = []
 
 async function create(client) {
+
   await client.connect();
   await client.db("pokedex").collection("pokemons").insertOne(pokeList[0]);
+  
 }
 
 
