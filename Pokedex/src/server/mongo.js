f12f5b87adc7b23451a9c898b3dc3e3deba01ef3
barshefet <bar.shefet@gmail.com>
2022-07-19 17:59:15 +0300

    trying stuff

---
+++ Pokedex/src/server/mongo.js
@@ -0,0 +1,16 @@
+const { MongoClient, Db } = require("mongodb");
+
+
+
+const uri = `mongodb+srv://barshefet:ilovecode@cluster0.rxb57.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+let pokeList = [];
+
+async function create(client) {
+  await client.connect();
+  await client.db("pokedex").collection("pokemons").insertOne(pokeList[0]);
+}
+
+
